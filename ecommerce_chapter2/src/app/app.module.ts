a7f64ed023e02405b778f3606121999ca4154876
Andrea D'Angiolillo <andrea.dangiolillo@gmail.com>
2023-03-12 17:31:48 +0100

    Primo es finito

--- ecommerce_chapter2/src/app/app.module.ts
+++ ecommerce_chapter2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
